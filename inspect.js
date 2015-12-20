f92b6bae69d6856712ace712f76b852166df2c7a
ktquez <ktquez@gmail.com>
2015-12-19 23:42:04 -0300

    Initial commit

---
+++ inspect.js
@@ -0,0 +1,14 @@
+/*
+*	inspect.js - Form validations in Javascript Vanilla, without dependencies and multiple languages
+*
+*	Copyright (c) 2015 Ktquez
+*
+*	Project repository: https://github.com/ktquez/Inspect
+*
+* 	Dual licensed under the MIT and GPL licenses.
+* 
+*/
+
+var inspect = function(){
+
+}
