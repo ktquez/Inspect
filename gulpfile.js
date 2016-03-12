bdd2899c0c5daafff5bc15b60b30b55414373568
ktquez <ktquez@gmail.com>
2016-03-12 19:25:32 -0300

    Organizing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
-var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-	jshint = require('gulp-jshint');
-	concat = require('gulp-concat');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
-gulp.task('dist', function(){
-	gulp.src('./dev/*.js')
+gulp.task('jshint', function(){
+	return gulp.src('./dev/*.js')
 		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(concat('inspect.min.js'))
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('clean', function(){
+	return gulp.src('./dist/inspect.min.js')
+		.pipe(clean());
+})
+
+gulp.task('dist', ['clean'], function(){
+	return gulp.src('./dev/*.js')
 		.pipe(uglify())
+		.pipe(concat('inspect.min.js'))
 		.pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('default', ['jshint', 'dist']);
